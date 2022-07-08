05741ccebb7c8a4d5b0c04070dce1a738c8593df
SYKnock <77342533+SYKnock@users.noreply.github.com>
2022-07-08 12:02:32 +0900

    Nvidia-apex

--- tools/train_classifier_distributed.py
+++ tools/train_classifier_distributed.py
@@ -2,7 +2,6 @@ import torch
 import wandb
 import torch.nn.functional as F
 import numpy as np
-from tqdm.notebook import tqdm
 import time
 from tools.eval import evaluation
 
@@ -11,7 +10,8 @@ def train(model, device, epoch, wandb_name,
           train_loader, valid_loader, test_loader, 
           optimizer, scheduler=None, interval=100):
 
-    wandb.init(project='mobilenetv3-small-p4d.24xlarge', entity='arguru', name=wandb_name)      
+    if device == 0:
+        wandb.init(project='mobilenetv3-small-distributed', entity='arguru', name=wandb_name)      
     start_time = time.time()
 
     
@@ -21,7 +21,7 @@ def train(model, device, epoch, wandb_name,
         train_total = 0
         epoch_loss_list = []
 
-        for batch_idx, (features, targets) in tqdm(enumerate(train_loader), total=len(train_loader)):
+        for batch_idx, (features, targets) in enumerate(train_loader):
             features = features.to(device)
             targets = targets.to(device)
             optimizer.zero_grad()
@@ -43,30 +43,31 @@ def train(model, device, epoch, wandb_name,
             train_total += targets.size(0)
 
             #logging
-            if batch_idx % interval == 0:
+            if (device == 0) and (batch_idx % interval == 0):
                 print(f'Epoch: {e+1:03d}/{epoch:03d}' 
                       f'| Batch: {batch_idx:04d}/{len(train_loader):04d}'
                       f' >> Loss: {loss:.04f}'
-                      f' [Time: {time.strftime("%Y-%m-%d %I:%M:%S %p", time.localtime())}]'
-                      f'| mp: {gpu}')
+                      f' [Time: {time.strftime("%Y-%m-%d %I:%M:%S %p", time.localtime())}]')
 
         valid_accuracy = evaluation(model, valid_loader, device)
         train_accuracy = round(train_correct / train_total * 100., 4)
         elapsed_time = (time.time() - start_time)
-        print(f'[[Epoch: {e+1:03d}/{epoch:03d}'
-              f'>> Train acc:{train_accuracy:.4f}% | Valid acc:{valid_accuracy:.4f}%'
-              f'| Elpased time: {elapsed_time:.2f}sec ({elapsed_time/60:.2f}min)]]'
-              f'| mp: {gpu}')
-        
-        if scheduler is not None:
+
+        if device == 0:
+            print(f'[[Epoch: {e+1:03d}/{epoch:03d}'
+                f'>> Train acc:{train_accuracy:.4f}% | Valid acc:{valid_accuracy:.4f}%'
+                f'| Elpased time: {elapsed_time:.2f}sec ({elapsed_time/60:.2f}min)]]')
             wandb.log({'Epoch':e, 'train/loss':round(np.mean(epoch_loss_list), 4), 
                        'var/accuracy_top-1':round(valid_accuracy, 4), 'learning_rate':scheduler.get_last_lr()[0]})
-            scheduler.step()
         
-    test_accuracy = evaluation(model, test_loader, device)
-    
-    total_time = (time.time() - start_time) / 60
-    print(f'======Test acc: {test_accuracy:.4f}% [Total time: {total_time:.2f}min]======\n\n')
+        scheduler.step()
+            
+
+    if device == 0: 
+        test_accuracy = evaluation(model, test_loader, device)
+        
+        total_time = (time.time() - start_time) / 60
+        print(f'======Test acc: {test_accuracy:.4f}% [Total time: {total_time:.2f}min]======\n\n')
 
     
     
