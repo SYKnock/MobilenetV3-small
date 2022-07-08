166ee7bec2a1dd2c445007f6307efc7f3287861e
SYKnock <77342533+SYKnock@users.noreply.github.com>
2022-07-08 12:00:45 +0900

    Nvidia-apex

--- MobilenetV3_CIFAR10_Distributed.py
+++ MobilenetV3_CIFAR10_Distributed.py
@@ -1,31 +1,37 @@
 import argparse
 import torch
-#from adamp import AdamP
 from tools.data_distributed import CIFAR10_distributed_dataloader_generator
 from tools.train_classifier_distributed import train
+from apex.parallel import DistribuedDataParallel as DDP
+import torch.multiprocessing as mp
 from tools.utils import set_seed
 import torchvision
 import torchvision.transforms as transforms
+import os
 
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
 
-def main_worker(gpu, ngpus_per_node, args):
+def main_worker(gpu, args):
+
+    torch.cuda.set_device(args.LOCAL_RANK) 
+    rank = gpu
 
     torch.distributed.init_process_group(
         backend='nccl',
-        init_method='tcp://127.0.0.1:4000',
-        world_size=ngpus_per_node,
-        rank=gpu)
+        init_method='env://',
+        world_size=args.WORLD_SIZE,
+        rank=rank)
+    
 
-    torch.cuda.set_device(gpu)
-    print("Use GPU: {} for training".format(gpu))
     model = torchvision.models.mobilenet_v3_small(weights=None)
     model.classifier[-1] = torch.nn.Linear(in_features=1024, out_features=10)
+    
+    torch.cuda.set_device(gpu)
     model = model.cuda(gpu)
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
 
+    model = DDP(model)
     params = model.parameters()
     lr = args.LR
 
@@ -36,7 +42,6 @@ def main_worker(gpu, ngpus_per_node, args):
 
     else:
         raise ValueError('Invalid optimizer choice')
-    #print(f"Optimizer INFO:{optimizer}")
 
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.9)
 
@@ -61,33 +66,33 @@ def main_worker(gpu, ngpus_per_node, args):
 
     set_seed(args.SEED)
 
-    if args.SCHEDULER is True:
-        train(model=model, device=gpu, epoch=args.EPOCHS,
-              train_loader=train_loader, valid_loader=valid_loader, test_loader=test_loader, 
-              optimizer=optimizer, scheduler=scheduler, wandb_name=args.WANDB_NAME)
-    else:
-        train(model=model, device=gpu, epoch=args.EPOCHS,
-              train_loader=train_loader, valid_loader=valid_loader, test_loader=test_loader, 
-              optimizer=optimizer, wandb_name=args.WANDB_NAME)
+
+    train(model=model, device=gpu, epoch=args.EPOCHS,
+            train_loader=train_loader, valid_loader=valid_loader, test_loader=test_loader, 
+            optimizer=optimizer, scheduler=scheduler, wandb_name=args.WANDB_NAME)
+
 
 
 
 
 def main():
-    parser = argparse.ArgumentParser(description='EPOCHS, BATCH_SIZE, NUM_WORKER, LR, OPTIMIZER, SCHEDULER, DEVICE, WANDB_NAME, SEED')
+    parser = argparse.ArgumentParser(description='EPOCHS, BATCH_SIZE, NUM_WORKER, LR, OPTIMIZER, SCHEDULER, DEVICE, WANDB_NAME, SEED, LOCAL_RANK, NUM_NODE')
     parser.add_argument('--EPOCHS', type=int, default=20, help='Number of epochs')
     parser.add_argument('--BATCH_SIZE', type=int, default=128, help='Batch size per chip')
     parser.add_argument('--NUM_WORKERS', type=int, default=2, help='Number of workers')
     parser.add_argument('--LR', type=float, default=1e-03, help='Learning rate')
     parser.add_argument('--OPTIMIZER', type=str, required=True, help='Select optimizer: Adam/RMSProp/AdamP')
-    parser.add_argument('--SCHEDULER', type=bool, default=True, help='True or False')
     parser.add_argument('--WANDB_NAME', type=str, required=True, help='Wandb logging name')
     parser.add_argument('--SEED', type=int, default=42, help='Random seed for initialization')
+    parser.add_argument('--LOCAL_RANK', type=int, default=0, help='Priority of node')
+    parser.add_argument('--NUM_NODE', type=int, default=1, help='Number of nodes in network')
     args = parser.parse_args()
 
     ngpus_per_node = torch.cuda.device_count()
-    
-    torch.multiprocessing.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
+    args.WORLD_SIZE = args.NUM_NODE * ngpus_per_node
+    os.environ['MASTER_ADDR'] = '127.0.0.1'
+    os.environ['MASTER_PORT'] = '30000'
+    mp.spawn(train, nprocs=ngpus_per_node, args=(args,))
 
 
 if __name__ == "__main__":
